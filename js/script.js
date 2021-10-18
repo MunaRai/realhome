b87a1d666ef1630d689ebff82d80fb9b18614bcf
Muna Rai <raimuna669@gmailcom>
2021-10-18 15:40:52 +0545

    client section responsive completed

--- js/script.js
+++ js/script.js
@@ -30,9 +30,6 @@ $(document).ready(function() {
         slidesToShow: 4,
         slidesToScroll: 1,
         autoplayspeed: 50
-        // prevArrow: '<span class="prev-arrow"><i class="fas fa-angle-left"></i></span>',
-        // nextArrow: '<span class="next-arrow"><i class="fas fa-angle-right"></i></span>',
-
     });
 
 });
