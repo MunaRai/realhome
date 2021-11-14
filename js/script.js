90d8e527272c8f4c35d9eff247fb2640b9df0d0a
Muna Rai <raimuna669@gmailcom>
2021-11-14 13:06:32 +0545

    changes made on testimonial section

--- js/script.js
+++ js/script.js
@@ -13,16 +13,16 @@ $('.carousel').carousel()
 //         autoplay: true,
 //     });
 
-//     $('#testimonial-slider').slick({
-//         dots: true,
-//         arrows: true,
-//         autoplay: true,
-//         prevArrow: '<span class="prev-arrow"><i class="fas fa-angle-left"></i></span>',
-//         nextArrow: '<span class="next-arrow"><i class="fas fa-angle-right"></i></span>',
-//         autoplayspeed: 1500,
-//         fade: true,
-//         fadespeed: 1000
-//     });
+    $('#testimonial-slider').slick({
+        dots: true,
+        arrows: true,
+        autoplay: true,
+        prevArrow: '<span class="prev-arrow"><i class="fas fa-angle-left"></i></span>',
+        nextArrow: '<span class="next-arrow"><i class="fas fa-angle-right"></i></span>',
+        autoplayspeed: 1500,
+        fade: true,
+        fadespeed: 1000
+    });
 
 //     $('#client-slider').slick({
 //         dots: false,
