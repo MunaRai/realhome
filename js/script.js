bff9b3bd91179799bb122a573f137393bab77d3a
Muna Rai <raimuna669@gmailcom>
2021-11-14 12:16:11 +0545

    worked on agent and more about us section

--- js/script.js
+++ js/script.js
@@ -1,50 +1,48 @@
-$(document).ready(function () {
-    $('#banner-slider').slick({
-        dots: true,
-        arrows: false,
-        autoplay: true,
-    });
+$('.carousel').carousel()
 
-    $('#agent-slider').slick({
-        dots: true,
-        arrows: false,
-        autoplay: true,
-    });
+// $(document).ready(function () {
+//     $('#banner-slider').slick({
+//         dots: true,
+//         arrows: false,
+//         autoplay: true,
+//     });
 
-    $('#testimonial-slider').slick({
-        dots: true,
-        arrows: true,
-        autoplay: true,
-        prevArrow: '<span class="prev-arrow"><i class="fas fa-angle-left"></i></span>',
-        nextArrow: '<span class="next-arrow"><i class="fas fa-angle-right"></i></span>',
-        autoplayspeed: 1500,
-        fade: true,
-        fadespeed: 1000
-    });
+//     $('#agent-slider').slick({
+//         dots: true,
+//         arrows: false,
+//         autoplay: true,
+//     });
 
-    $('#client-slider').slick({
-        dots: false,
-        arrows: false,
-        autoplay: true,
-        Infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        autoplayspeed: 50,
-        responsive: [
-            {
-                breakpoint: 576,
-                settings: {
-                    // arrows: false,
-                    // dots: false,
-                    // autoplay: true,
-                    // Infinite: true,
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
+//     $('#testimonial-slider').slick({
+//         dots: true,
+//         arrows: true,
+//         autoplay: true,
+//         prevArrow: '<span class="prev-arrow"><i class="fas fa-angle-left"></i></span>',
+//         nextArrow: '<span class="next-arrow"><i class="fas fa-angle-right"></i></span>',
+//         autoplayspeed: 1500,
+//         fade: true,
+//         fadespeed: 1000
+//     });
 
-});
+//     $('#client-slider').slick({
+//         dots: false,
+//         arrows: false,
+//         autoplay: true,
+//         Infinite: true,
+//         slidesToShow: 4,
+//         slidesToScroll: 4,
+//         autoplayspeed: 50,
+//         responsive: [
+//             {
+//                 breakpoint: 576,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1
+//                 }
+//             }
+//         ]
+//     });
+
+// });
 
 
